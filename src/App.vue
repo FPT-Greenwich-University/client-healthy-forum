23a320d8105702400f9a5c28b1864aa13345b437
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-02-24 14:41:04 +0700

    update top nav

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,58 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      color="light-green darken-1"
-      dark
+        app
+        color="light-green darken-1"
+        dark
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
         />
 
         <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
         />
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <div class="text-center">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                color="primary"
+                dark
+                v-bind="attrs"
+                v-on="on"
+            >
+              Menu
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+            >
+              <v-list-item-title>Item 1</v-list-item-title>
+            </v-list-item>
+          </v-list>
+          <v-list>
+            <v-list-item
+            >
+              <v-list-item-title>Item 2 sdfsdfsdfsdfsdf</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
     </v-app-bar>
 
     <v-main>
