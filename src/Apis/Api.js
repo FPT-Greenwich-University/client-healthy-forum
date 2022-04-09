ea0d6b2badcf86837c4ef2b42657e0a4f18cbba2
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-04-09 09:05:40 +0700

    update Api axios file

--- src/Apis/Api.js
+++ src/Apis/Api.js
@@ -5,11 +5,10 @@ const BaseApi = axios.create({
 });
 
 BaseApi.defaults.headers.post["Content-Type"] = "application/json";
-BaseApi.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 const Api = () => {
     const token = localStorage.getItem("token");
-    // console.log('url', process.env.VUE_APP_BACKEND_API_URL)
+
     if (token) {
         BaseApi.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     }
